54082c7563bd4faff4fd9cad4c11679eef5525f6
StrkerXx798 <str1k4@yandex.ru>
2020-04-26 18:28:44 +0300

    Add files 41 lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Post from './Post/Post';
 import classes from './MyPosts.module.css';
-import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/state"
+import {addPostActionCreator, updateNewPostActionCreator} from "../../../redux/profile-reducer"
 
 const MyPosts = (props) => {
 
