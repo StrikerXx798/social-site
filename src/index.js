cb30f3db115159dc5ff1ead50e5857d2ddc9234c
StrkerXx798 <str1k4@yandex.ru>
2020-04-26 10:45:35 +0300

    Add files 37 lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntireTree } from './render';
-import state from './redux/state'
+import store from "./redux/state"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-rerenderEntireTree (state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText = {store.updateNewPostText.bind(store)} />
+        </BrowserRouter>, document.getElementById('root'));
+}
+
+rerenderEntireTree (store.getState());
+
+
+store.subscribe (rerenderEntireTree);
 
 serviceWorker.unregister();
